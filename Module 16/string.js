d5c9cfecf7c5082d3a614e4ff9264b7de2334865
samiul-hasan-sysforge <samiulh085@gmail.com>
2026-01-30 01:07:14 +0600

    js-object-&-string-tasks

--- Module 16/string.js
+++ Module 16/string.js
@@ -192,6 +192,5 @@ for (let i = 0; i < words.length; i++) {
         words[i] = words[i][0].toUpperCase() + words[i].slice(1);
     }
 }
-
 const capitalizedSentence = words.join(' ');
 console.log(capitalizedSentence);
